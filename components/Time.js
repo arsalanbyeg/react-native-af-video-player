58a568f766b180e816ab34c685900b4614e37f56
arfa123 <aphahmed@yahoo.com>
2020-09-14 20:56:26 +0500

    vide time hiding in ios fix

--- components/Time.js
+++ components/Time.js
@@ -1,48 +1,48 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 const backgroundColor = "transparent";
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor,
-    justifyContent: "center",
-    padding: 10,
-    minWidth: 60
-  }
+	container: {
+		alignItems: "center",
+		backgroundColor,
+		justifyContent: "center",
+		padding: Platform.OS === "ios" ? 0 : 10,
+		minWidth: 60
+	}
 });
 
 class Time extends Component {
-  getTime(time) {
-    // format the seconds saved into 00:00:00
-    const secs = time % 60;
-    const s2 = (time - secs) / 60;
-    const mins = s2 % 60;
-    const hrs = (s2 - mins) / 60;
-    const hours = this.addZeros(hrs) > 0 ? `${this.addZeros(hrs)}:` : "";
-    return `${hours}${this.addZeros(mins)}:${this.addZeros(secs)}`;
-  }
+	getTime(time) {
+		// format the seconds saved into 00:00:00
+		const secs = time % 60;
+		const s2 = (time - secs) / 60;
+		const mins = s2 % 60;
+		const hrs = (s2 - mins) / 60;
+		const hours = this.addZeros(hrs) > 0 ? `${this.addZeros(hrs)}:` : "";
+		return `${hours}${this.addZeros(mins)}:${this.addZeros(secs)}`;
+	}
 
-  addZeros(time) {
-    return time < 10 ? `0${time}` : time;
-  }
+	addZeros(time) {
+		return time < 10 ? `0${time}` : time;
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{ color: this.props.theme }}>
-          {this.getTime(parseInt(this.props.time, 10))}
-        </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={{ color: this.props.theme }}>
+					{this.getTime(parseInt(this.props.time, 10))}
+				</Text>
+			</View>
+		);
+	}
 }
 
 Time.propTypes = {
-  time: PropTypes.number.isRequired,
-  theme: PropTypes.string.isRequired
+	time: PropTypes.number.isRequired,
+	theme: PropTypes.string.isRequired
 };
 
 export { Time };
